7a17264073f107bb9c8cb3fcc1e044e2226d0954
Kesavan.P <kesavanp0395@gmail.com>
2025-03-10 11:28:36 +0530

    Delete src directory

--- src/store.js
+++
@@ -1,9 +0,0 @@
-// src/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import cryptoReducer from './features/cryptoSlice';
-
-export default configureStore({
-  reducer: {
-    crypto: cryptoReducer,
-  },
-});
